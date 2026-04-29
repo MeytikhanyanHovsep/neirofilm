f8b3e2768b141f00a07077607201e29ed78c9a46
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2026-04-29 19:09:31 +0400

    fixed services bugs

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,3 +1,5 @@
+import Cases from "@/components/sections/cases";
+import { Case } from "@/components/sections/cases/casesSlider";
 import GetInTouch from "@/components/sections/getInTouch";
 import SecondHero from "@/components/sections/secondHero";
 import ServicesSection from "@/components/sections/servicesSection";
@@ -5,10 +7,27 @@ import ServicesSection from "@/components/sections/servicesSection";
 type Props = {};
 
 export default function Services({}: Props) {
+  const cases: Case[] = [
+    { video: "/videos/case-1.mp4", title: "AI-реклама бренда" },
+    { video: "/videos/case-1.mp4", title: "AI-реклама бренда" },
+    { video: "/videos/case-1.mp4", title: "AI-реклама бренда" },
+    { video: "/videos/case-1.mp4", title: "AI-реклама бренда" },
+    { video: "/videos/case-1.mp4", title: "AI-реклама бренда" },
+    { video: "/videos/case-1.mp4", title: "AI-реклама бренда" },
+  ];
   return (
     <>
       <SecondHero />
-      <ServicesSection styles="lg:pt-[120px] lg:mb-20  pt-[80px]" />
+      <Cases
+        title={
+          <>
+            ИИ-видео под ключ — <br className="min-[510px]:hidden" /> масштабный
+            визуал без съёмок
+          </>
+        }
+        type="grid"
+        cases={cases}
+      />
       <GetInTouch />
     </>
   );
